f257d8372b275246462e9565bc4c6e02fc576e33
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2025-07-04 14:22:39 +0100

    corrige nome do CSS para home.module.css

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-  images: {
-    domains: ["lh3.googleusercontent.com"], // Adicione outros domínios se necessário
-  },
 };
 
 export default nextConfig;
